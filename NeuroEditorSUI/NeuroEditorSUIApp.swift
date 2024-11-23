5d799403d9861a8ddc8b9e87a4e9a5979857d659
Natali <ns@numbuster.com>
2024-11-23 13:28:17 +0300

    Initial Commit

---
+++ NeuroEditorSUI/NeuroEditorSUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NeuroEditorSUIApp.swift
+//  NeuroEditorSUI
+//
+//  Created by Natalia Sinitsyna on 23.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct NeuroEditorSUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
